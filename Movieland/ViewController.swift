dbbef0703b5f87a935e2243b227b7f434134cb16
Cenker Irmak <irmakcenker@gmail.com>
2021-03-02 00:34:27 +0300

    Initial Commit

---
+++ Movieland/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movieland
+//
+//  Created by Cenker Irmak on 2.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
