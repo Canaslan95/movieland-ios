be3660dcd3e58680506836506ccb472c82afcd8d
Cenker Irmak <irmakcenker@gmail.com>
2021-03-25 22:04:40 +0300

    assetler güncellendi

--- Movieland/Helpers/CustomTabBarController.swift
+++ Movieland/Helpers/CustomTabBarController.swift
@@ -22,7 +22,8 @@ class CustomTabBarController: UITabBarController {
         // MovieViewController
         let moviesRootController = MoviesViewController(nibName: "MoviesViewController", bundle: .main)
         moviesRootController.tabBarItem.title = "Movies"
-        moviesRootController.tabBarItem.image = #imageLiteral(resourceName: "movie_tab_image").withRenderingMode(.alwaysOriginal)
+        moviesRootController.tabBarItem.image = #imageLiteral(resourceName: "movie_tab_unselected").withRenderingMode(.alwaysOriginal)
+        moviesRootController.tabBarItem.selectedImage = #imageLiteral(resourceName: "movie_tab_selected").withRenderingMode(.alwaysOriginal)
         let moviesNavigationController = UINavigationController(rootViewController: moviesRootController)
  
         
@@ -30,7 +31,8 @@ class CustomTabBarController: UITabBarController {
         let tvSeriesRootController = UIViewController()
         tvSeriesRootController.view.backgroundColor = .red
         tvSeriesRootController.tabBarItem.title = "TV Series"
-        tvSeriesRootController.tabBarItem.image = #imageLiteral(resourceName: "tv_series_tab_iamge").withRenderingMode(.alwaysOriginal)
+        tvSeriesRootController.tabBarItem.image = #imageLiteral(resourceName: "series_tab_unselected").withRenderingMode(.alwaysOriginal)
+        tvSeriesRootController.tabBarItem.selectedImage = #imageLiteral(resourceName: "series_tab_selected").withRenderingMode(.alwaysOriginal)
         
         viewControllers = [moviesNavigationController, tvSeriesRootController]
     }
@@ -41,14 +43,14 @@ class CustomTabBarController: UITabBarController {
         tabBar.unselectedItemTintColor = .black
 
         for item in tabBarItems {
-            item.setTitleTextAttributes([.foregroundColor: UIColor.gray], for: .normal)
-            item.setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+            item.setTitleTextAttributes([.foregroundColor: UIColor.black], for: .normal)
+            item.setTitleTextAttributes([.foregroundColor: UIColor.blue], for: .selected)
         }
         
     }
 
     func configureTabBarUI() {
-        tabBar.barTintColor = .white
+        tabBar.barTintColor = UIColor(red: 28, green: 46, blue: 30, alpha: 1)
         tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
         tabBar.layer.shadowRadius = 3
         tabBar.layer.shadowColor = UIColor.orange.cgColor
